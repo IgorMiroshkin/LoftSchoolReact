3468ec6aad9ca6aa9d7180ae5983e00fc89fb034
KPACHb1Y <barb0s9109@mail.ru>
2019-10-27 01:42:32 +0300

    create first step in project

---
+++ src/map/Map.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const MapPage = ({ setPage }) => {
+  return(
+      <>
+          <h1>Map</h1>
+      </>
+  );
+};
---
+++ src/map/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { MapPage } from './Map';
+
+export const Map = ({ setPage }) => {
+  return(
+      <>
+          <MapPage />
+      </>
+  );
+};
